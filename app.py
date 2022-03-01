d7a25487c9d1125c24304905994621636b0d37c3
Stephanie Kasaon <stephaniekasaon@gmail.com>
2022-03-01 15:58:04 +0900

    deleted some unnecessary files

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template, flash, request, url_for, redirect, session
+from flask import Flask, render_template, request
 import numpy as np
-import pandas as pd
 import re
 import os
-import tensorflow as tf
 from numpy import array
 from keras.datasets import imdb
 from keras.preprocessing import sequence
